99471bcee36d084054767ba64c772fd2873bc45b
ErinKace <erinkacerovskis@gmail.com>
2023-07-26 19:36:01 -0500

    keep forgetting to commit when Im partway done

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -23,31 +23,34 @@ public class SpaDayController {
             return true;
         }
     }
-
-    @GetMapping(value="")
-    @ResponseBody
-    public String customerForm () {
-        String html = "<form method = 'post'>" +
-                "Name: <br>" +
-                "<input type = 'text' name = 'name'>" +
-                "<br>Skin type: <br>" +
-                "<select name = 'skintype'>" +
-                "<option value = 'oily'>Oily</option>" +
-                "<option value = 'combination'>Combination</option>" +
-                "<option value = 'normal'>Normal</option>" +
-                "<option value = 'dry'>Dry</option>" +
-                "</select><br>" +
-                "Manicure or Pedicure? <br>" +
-                "<select name = 'manipedi'>" +
-                "<option value = 'manicure'>Manicure</option>" +
-                "<option value = 'pedicure'>Pedicure</option>" +
-                "</select><br>" +
-                "<input type = 'submit' value = 'Submit'>" +
-                "</form>";
-        return html;
+    @GetMapping("")
+    public String index() {
+        return "index";
     }
 
-    @PostMapping(value="")
+    @GetMapping("/form")
+    public String customerForm () {
+//        String html = "<form method = 'post'>" +
+//                "Name: <br>" +
+//                "<input type = 'text' name = 'name'>" +
+//                "<br>Skin type: <br>" +
+//                "<select name = 'skintype'>" +
+//                "<option value = 'oily'>Oily</option>" +
+//                "<option value = 'combination'>Combination</option>" +
+//                "<option value = 'normal'>Normal</option>" +
+//                "<option value = 'dry'>Dry</option>" +
+//                "</select><br>" +
+//                "Manicure or Pedicure? <br>" +
+//                "<select name = 'manipedi'>" +
+//                "<option value = 'manicure'>Manicure</option>" +
+//                "<option value = 'pedicure'>Pedicure</option>" +
+//                "</select><br>" +
+//                "<input type = 'submit' value = 'Submit'>" +
+//                "</form>";
+        return "form";
+    }
+
+    @PostMapping("/form")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
         ArrayList<String> facials = new ArrayList<>();
@@ -62,6 +65,10 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
+        model.addAttribute("appropriateFacials",appropriateFacials);
+        model.addAttribute("name", name);
+        model.addAttribute("skinType", skintype);
+        model.addAttribute("manipedi", manipedi);
 
         return "menu";
     }
